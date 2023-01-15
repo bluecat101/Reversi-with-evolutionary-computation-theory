7ba8c179a10637cc90a2a24a06f5c9a5a8eb59ab
s2110290 <s2110290@gl.cc.uec.ac.jp>
2023-01-15 16:09:10 +0900

    a

--- ReversiController.java
+++ ReversiController.java
@@ -5,27 +5,22 @@ import javax.swing.border.LineBorder;
 import java.util.*;
 import java.nio.charset.StandardCharsets;
 
-
 // Controller (C)
 
 // KeyListener が，キー操作のリスナーインタフェース．
-class ReversiController implements KeyListener, MouseListener, MouseMotionListener, ActionListener{
+class ReversiController implements KeyListener, MouseListener, MouseMotionListener, ActionListener {
   protected Model model;
   protected Model.ReversiModel reversiModel;
   protected Model.ChatModel chatModel;
   protected ReversiView view;
-  private int num=0;
-  private int test_player = 1;
+  private int num = 0;
   private javax.swing.Timer timer;
-  //add||||||||||||||||||||
-  private String mode;
-  private CommClient cl;  
-  //||||||||||||||||||||
-  //消す
-  private Model ai;//aiのクラスを保持
-  //--------------------
+  private String mode;// Ai,client,server&client
+  private CommClient cl;// client
+  private CommServer sv;// server
+  private Model ai;// aiのクラスを保持
 
-  public ReversiController(Model m, ReversiView v){
+  public ReversiController(Model m, ReversiView v) {
     model = m;
     reversiModel = model.getReversiModel();
     chatModel = model.getChatModel();
@@ -38,170 +33,185 @@ class ReversiController implements KeyListener, MouseListener, MouseMotionListen
     view.getResetButton().addActionListener(this);
     view.getChatButton().addActionListener(this);
     view.getChatBox().addActionListener(this);
-    //いらないかな
-    // view.getResetButton().addKeyListener(this);
     view.getChatButton().addKeyListener(this);
-    //add||||||||||||||||||||
     view.getSingleStartButton().addActionListener(this);
     view.getMultiPasswordBox().addActionListener(this);
-
-    //||||||||||||||||||||
   }
-  public void actionPerformed(ActionEvent e){
-    //edit""""""""""""""
-    if(e.getSource()==timer){
-      if(mode.contains("Ai")){
+
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == timer) {
+      if (mode.contains("Ai")) {
         ai.run();
-        if(reversiModel.getPassFlag("controller")==0){//現状がpassじゃない<=>自分のターンに移る
+        if (reversiModel.getPassFlag("controller") == 0) {// 現状がpassじゃない<=>自分のターンに移る
           view.getPanel().requestFocus();
-          timer.stop(); 
+          timer.stop();
         }
         view.getPanel().requestFocus();
         timer.stop();
-      }else if(mode=="server&client"){
+      } else if (mode == "server&client") {
         String msg;
-        if ((msg=reversiModel.getServer().recv())!=null)  {//クライアントからの変化があるかを確認する
-          reversiModel.getServer().send(msg);//変化をサーバーに送信
+        if ((msg = sv.recv()) != null) {// クライアントからの変化があるかを確認する
+          sv.send(msg);// 変化をサーバーに送信
         }
-      }else if(mode=="client"){
+      } else if (mode == "client") {
         cl.recv();
       }
     }
-    //"""""""""""""""""""""""""""""
-    if(e.getSource() == view.getResetButton()){
+    if (e.getSource() == view.getResetButton()) {
       reversiModel.initBoard();
-    }else if(e.getSource() == view.getChatButton()){
-      if(num%2==1){
-        view.getChatBox().setEnabled(false); num--;
-      }else{
+    } else if (e.getSource() == view.getChatButton()) {
+      if (num % 2 == 1) {
+        view.getChatBox().setEnabled(false);
+        num--;
+      } else {
         view.getChatBox().setEnabled(true);
-        view.getChatBox().grabFocus(); num++;
+        view.getChatBox().grabFocus();
+        num++;
       }
-    }else if(e.getSource() == view.getChatBox()){
+    } else if (e.getSource() == view.getChatBox()) {
       String chat_sentence = view.getChatBox().getText();
-      if(chat_sentence.equals("")){
-        view.getChatBox().setEnabled(false); num--;
-      }else{
-        chatModel.setChat(chat_sentence, test_player);
-        System.out.println(chat_sentence);
-        if(test_player == 1){test_player++;}
-        else{test_player--;}
+      if (chat_sentence.equals("")) {
+        view.getChatBox().setEnabled(false);
+        num--;
+      } else {
+        if (mode == "server&client") {
+          sv.sendChat(chat_sentence, chatModel.getPlayerNum());
+        } else if (mode == "client") {
+          cl.sendChat(chat_sentence, chatModel.getPlayerNum());
+        }
         view.getChatBox().setText("");
       }
     }
-    
-   //add||||||||||||||||||||||
-    if(e.getSource()==view.getSingleStartButton()){
-      
-      if(!Objects.isNull(view.getAiMode())){//aiが押されているかを判定
+    if (e.getSource() == view.getSingleStartButton()) {
+
+      if (!Objects.isNull(view.getAiMode())) {// aiが押されているかを判定
         System.out.println("ai");
-        ai=view.getAiMode();
-        mode="Ai";
-        timer =new javax.swing.Timer(2,this);//aiが2秒後に行動する。
+        ai = view.getAiMode();
+        mode = "Ai";
+        timer = new javax.swing.Timer(2, this);// aiが2秒後に行動する。
       }
-    }else if(e.getSource()==view.getMultiPasswordBox()){
-      if(view.getSVMode()){
-        System.out.println("server");
-        int port=getPort(view.getMultiPasswordBox().getText());//合言葉をポート番号に変換する。
-        mode="server&client";
-        reversiModel.newServer(port);//サーバーの設立
-      }else{
-        System.out.println("client");
-        mode="client";
-        int port=getPort(view.getMultiPasswordBox().getText());//合言葉をポート番号に変換する。
-        cl = new CommClient("localhost",port,this.reversiModel);//クライアントの確立
-        cl.setTimeout(10);//タイムアウトを設定
-        reversiModel.changeIsYourTurn();
-        chatModel.changePlayerNum();
+    } else if (e.getSource() == view.getMultiPasswordBox()) {
+      if (view.getSVMode()) {
+        int port = getPort(view.getMultiPasswordBox().getText());// 合言葉をポート番号に変換する。
+        mode = "server&client";
+        sv = new CommServer(port, model);// サーバーの設立
+        sv.setTimeout(10);// タイムアウト時間の設定
+      } else {
+        mode = "client";
+        int port = getPort(view.getMultiPasswordBox().getText());// 合言葉をポート番号に変換する。
+        cl = new CommClient("localhost", port, model);// クライアントの確立
+        cl.setTimeout(10);// タイムアウトを設定
+        reversiModel.changeIsYourTurn();// クライアントを後攻とする
+        chatModel.changePlayerNum();// chatの番号をクライアントを２番目であるとする
       }
-      timer =new javax.swing.Timer(300,this);//0.3秒ごとにクライアントかサーバーで変化があったのかを確認する。
-        timer.start();//変化の探査開始
+      timer = new javax.swing.Timer(300, this);// 0.3秒ごとにクライアントかサーバーで変化があったのかを確認する。
+      timer.start();// 変化の探査開始
     }
-    //||||||||||||||||||||||||||||||
   }
-  private int getPort(String password){
-    int port=0;//port番号
-    for(int i=password.getBytes(StandardCharsets.US_ASCII).length-1;0<=i;port+=password.getBytes(StandardCharsets.US_ASCII)[i--]){}//port番号をpasswordから計算
-    //↑合言葉の全ての文字をそれぞれACSIIコードに変換してそれを足す。
-    //↓合言葉をint型にしたものを9973(一万以下の最大の素数)で余りを取って50000を足す。
-    port=port%9973+50000;//port番号を算出
+
+  private int getPort(String password) {
+    int port = 0;// port番号
+    for (int i = password.getBytes(StandardCharsets.US_ASCII).length - 1; 0 <= i; port += password
+        .getBytes(StandardCharsets.US_ASCII)[i--]) {
+    } // port番号をpasswordから計算
+    // ↑合言葉の全ての文字をそれぞれACSIIコードに変換してそれを足す。
+    // ↓合言葉をint型にしたものを9973(一万以下の最大の素数)で余りを取って50000を足す。
+    port = port % 9973 + 50000;// port番号を算出
     return port;
   }
-  public void mouseDragged(MouseEvent e){}
-  public void mouseMoved(MouseEvent e){
+
+  public void mouseDragged(MouseEvent e) {
+  }
+
+  public void mouseMoved(MouseEvent e) {
     reversiModel.next_position_mouse(e.getX(), e.getY());
   }
-  public void mouseClicked(MouseEvent e){}
-  public void mouseEntered(MouseEvent e){}
-  public void mouseExited(MouseEvent e){}
-  public void mousePressed(MouseEvent e){
-    if(e.getSource() == view.getPanel() && reversiModel.transformMousePoint(e.getX()) == reversiModel.getPikaPika_x() && reversiModel.transformMousePoint(e.getY()) == reversiModel.getPikaPika_y()){
-      if(mode.contains("Ai")){
-        reversiModel.xySetStone(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y());
-        timer.start();
-        view.getChatBox().setEnabled(true);
-        view.getChatBox().grabFocus(); num++;
-      }else if(mode=="server&client"){
-        reversiModel.getServer().send(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y());//サーバーに直接送る
-      }else if(mode=="client"){
-        cl.send(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y());//クライアントからサーバーに送る
-      }
 
-    }/*else if(e.getSource() == view.getChatPanel()){
-      System.out.println("chat");
-      if(num%2==1){
-        view.getChatBox().setEnabled(false); num--;
-      }else{
-        view.getChatBox().setEnabled(true);
-        view.getChatBox().grabFocus(); num++;
-      }
-    }*/
+  public void mouseClicked(MouseEvent e) {
   }
-  public void mouseReleased(MouseEvent e){}
-  public void keyTyped(KeyEvent e){
-    char c = e.getKeyChar();
-    switch(c){
-      case 'z':
-      if(mode.contains("Ai")){
-        reversiModel.xySetStone(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y());
+
+  public void mouseEntered(MouseEvent e) {
+  }
+
+  public void mouseExited(MouseEvent e) {
+  }
+
+  public void mousePressed(MouseEvent e) {
+    if (e.getSource() == view.getPanel() && reversiModel.transformMousePoint(e.getX()) == reversiModel.getPikaPika_x()
+        && reversiModel.transformMousePoint(e.getY()) == reversiModel.getPikaPika_y()) {
+      if (mode.contains("Ai")) {
+        reversiModel.xySetStone(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y());
         timer.start();
         view.getChatBox().setEnabled(true);
-        view.getChatBox().grabFocus(); num++;
-      }else if(mode=="server&client"){
-        reversiModel.getServer().send(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y());//サーバーに直接送る
-      }else if(mode=="client"){
-        cl.send(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y());//クライアントからサーバーに送る
+        view.getChatBox().grabFocus();
+        num++;
+      } else if (mode == "server&client") {
+        sv.send(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y());// サーバーに直接送る
+      } else if (mode == "client") {
+        cl.send(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y());// クライアントからサーバーに送る
       }
 
-      break;
-      case 'a':
-      reversiModel.changeIsYourTurn();
-      break;
+    } /*
+       * else if(e.getSource() == view.getChatPanel()){
+       * System.out.println("chat");
+       * if(num%2==1){
+       * view.getChatBox().setEnabled(false); num--;
+       * }else{
+       * view.getChatBox().setEnabled(true);
+       * view.getChatBox().grabFocus(); num++;
+       * }
+       * }
+       */
+  }
+
+  public void mouseReleased(MouseEvent e) {
+  }
+
+  public void keyTyped(KeyEvent e) {
+    char c = e.getKeyChar();
+    switch (c) {
+      case 'z':
+        if (mode.contains("Ai")) {
+          reversiModel.xySetStone(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y());
+          timer.start();
+          view.getChatBox().setEnabled(true);
+          view.getChatBox().grabFocus();
+          num++;
+        } else if (mode == "server&client") {
+          sv.send(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y());// サーバーに直接送る
+        } else if (mode == "client") {
+          cl.send(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y());// クライアントからサーバーに送る
+        }
+        break;
     }
   }
- public void keyPressed(KeyEvent e){
+
+  public void keyPressed(KeyEvent e) {
     int k = e.getKeyCode();
-    switch(k){
+    switch (k) {
       case KeyEvent.VK_RIGHT:
-        //下四つは盤面の移動を矢印キーでやる場合に使う
-        reversiModel.next_position(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y(),0);//0:右,1:左,2:下,3:上
+        // 下四つは盤面の移動を矢印キーでやる場合に使う
+        reversiModel.next_position(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y(), 0);// 0:右,1:左,2:下,3:上
         break;
       case KeyEvent.VK_LEFT:
-        reversiModel.next_position(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y(),1);
+        reversiModel.next_position(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y(), 1);
         break;
       case KeyEvent.VK_UP:
-        reversiModel.next_position(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y(),3);
+        reversiModel.next_position(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y(), 3);
         break;
       case KeyEvent.VK_DOWN:
-        reversiModel.next_position(reversiModel.getPikaPika_x(),reversiModel.getPikaPika_y(),2);
+        reversiModel.next_position(reversiModel.getPikaPika_x(), reversiModel.getPikaPika_y(), 2);
         break;
       case KeyEvent.VK_ENTER:
         view.getChatBox().setEnabled(true);
-        view.getChatBox().grabFocus(); num++;
+        view.getChatBox().grabFocus();
+        num++;
         break;
       default:
         break;
     }
   }
-  public void keyReleased(KeyEvent e){}
+
+  public void keyReleased(KeyEvent e) {
+  }
 }
